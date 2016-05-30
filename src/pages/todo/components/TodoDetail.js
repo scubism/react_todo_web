c00e8edd8a016f49ebd636d903ef00b8cc180e7f
vinhkq <k_vinh@stagegroup.jp>
2016-05-30 10:06:26 +0700

    Resolve feedback 30/5

--- src/pages/todo/components/TodoDetail.js
+++ src/pages/todo/components/TodoDetail.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { VIEW_TODO } from '../actions';
 
 @provideHooks({
-  fetch: ({ dispatch, params: { todoId } }) => dispatch({'type': VIEW_TODO.REQUEST, id: todoId})
+  fetch: ({ dispatch, params: { todoId } }) => dispatch({type: VIEW_TODO.REQUEST, id: todoId})
 })
 @connect((state) => {
   return {
@@ -14,22 +14,25 @@ import { VIEW_TODO } from '../actions';
 
 class TodoDetail extends React.Component {
 
+  _renderEmpty() {
+    return (
+      <div className="todo-detail">
+      </div>
+    );
+  }
   render() {
     const { todo } = this.props;
-    if(todo!==null) {
-      return (
-        <div className="todo-detail">
-          <p>ID: {todo.id}</p>
-          <p>Title: {todo.title}</p>
-          <p>Due Date: {todo.due_date}</p>
-          <p>Sort Order: {todo.sort_order}</p>
-          <p>Group Id: {todo.todo_groups_id}</p>
-          <p>Marked: {todo.marked}</p>
-        </div>
-      )
+    if(!todo) {
+      return this._renderEmpty.bind(this)();
     }
     return (
       <div className="todo-detail">
+        <p>ID: {todo.id}</p>
+        <p>Title: {todo.title}</p>
+        <p>Due Date: {todo.due_date}</p>
+        <p>Sort Order: {todo.sort_order}</p>
+        <p>Group Id: {todo.todo_groups_id}</p>
+        <p>Marked: {todo.marked}</p>
       </div>
     );
   }
