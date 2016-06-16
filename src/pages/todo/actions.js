1e706a6d8008eae6e6ae4854a5fdbd407f3c1a99
vinhkq <k_vinh@stagegroup.jp>
2016-06-16 09:59:53 +0700

    delete remain file

--- src/pages/todo/actions.js
+++
@@ -1,8 +0,0 @@
-import { createRequestTypes } from '../../common/api'
-
-export const LIST_TODOS = createRequestTypes('LIST_TODOS');
-export const MOVE_TODO = createRequestTypes('MOVE_TODO');
-export const VIEW_TODO = createRequestTypes('VIEW_TODO');
-export const CREATE_TODO = createRequestTypes('CREATE_TODO');
-export const UPDATE_TODO = createRequestTypes('UPDATE_TODO');
-export const DELETE_TODO = createRequestTypes('DELETE_TODO');
