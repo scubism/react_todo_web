ebdb8592bdeb66b9f7cca9e2772ed23e5b5acb64
Kenji Hosoda <hosoda@s-cubism.jp>
2016-06-21 15:25:08 +0900

    Remove login module for simplicity with better home design

--- src/common/components/Routes.js
+++ src/common/components/Routes.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './App';
-import LoginPage from '../../modules/login/containers';
-
 import HomePage from '../../modules/home/containers';
-
 import TodoPage from '../../modules/todo/containers';
 import TodoList from '../../modules/todo/components/TodoList';
 import TodoDetail from '../../modules/todo/components/TodoDetail';
@@ -13,8 +10,7 @@ import TodoDetail from '../../modules/todo/components/TodoDetail';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={LoginPage} />
-    <Route path="home" component={HomePage} />
+    <IndexRoute component={HomePage} />
     <Route path="/todos" component={TodoPage}>
       <IndexRoute component={TodoList}/>
       <Route path="/todos/:todoId" component={TodoDetail}/>
--- src/modules/home/containers.js
+++ src/modules/home/containers.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { Link } from 'react-router'
 
 if (process.env.BROWSER) {
   require("./home.css");
 }
 
-
-
 export default class HomePage extends React.Component {
   render() {
     return (
       <div className="home">
-        <h1>Home Page</h1>
-        <p className="welcomeText">Thanks for joining!</p>
+        <h1>Todo Center</h1>
+        <ul>
+          <li>
+            <Link to="/todos">Try Todo Sample</Link>
+          </li>
+        </ul>
+        <p className="caption">
+          This project is a sample prototype to build microservices.
+          <br/>
+          See the gitlab repository for details:
+          <a target="_blank" href="https://github.com/scubism/todo_center">https://github.com/scubism/todo_center</a>
+        </p>
       </div>
     );
   }
