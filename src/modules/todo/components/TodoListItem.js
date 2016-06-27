14c21ce085b7471c7820fc8f9af001991f76fffc
Kenji Hosoda <hosoda@s-cubism.jp>
2016-06-27 11:35:05 +0900

    Implement mark update for todos

--- src/modules/todo/components/TodoListItem.js
+++ src/modules/todo/components/TodoListItem.js
@@ -2,16 +2,25 @@ import React from 'react'
 import { Link } from 'react-router'
 import Loader from 'react-loaders'
 
-import { focusTodo, deleteTodo } from '../actions';
+import { updateTodo, deleteTodo, focusTodo } from '../actions';
 
 export default class TodoListItem extends React.Component {
+  _onMarkChange() {
+    const { todo, dispatch } = this.props;
+    dispatch(updateTodo({
+      id: todo.id,
+      data: Object.assign({}, todo, {marked: !todo.marked ? 1 : 0}),
+      resolve: null,
+      reject: () => { alert('update failed!'); }
+    }));
+  }
 
   render() {
     const { todo, dispatch } = this.props;
-
     return (
       <li className={todo.marked ? 'completed' : ''}>
-        <input className="toggle" type="checkbox" />
+        <input className="toggle" type="checkbox" checked={todo.marked}
+          onChange={this._onMarkChange.bind(this)} />
         <label onClick={() => {dispatch(focusTodo(todo))}}>{todo.title}</label>
         <button className="destroy" onClick={() => {dispatch(deleteTodo({id: todo.id}))}} />
       </li>
